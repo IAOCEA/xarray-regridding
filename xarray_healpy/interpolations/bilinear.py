e8550488df9e97b89d434778fa008a80faba8d5d
Justus Magin <keewis@posteo.de>
2023-11-23 11:31:08 +0100

    add a high-level wrapper for the weights computation

--- xarray_healpy/interpolations/bilinear.py
+++ xarray_healpy/interpolations/bilinear.py
@@ -1,5 +1,12 @@
+import itertools
+
 import numba
 import numpy as np
+import sparse
+import xarray as xr
+
+# TODO: replace with a scipy KDTree
+from sklearn.neighbors import BallTree
 
 
 @numba.njit
@@ -113,3 +120,83 @@ def _compute_bilinear_interpolation_weights(
         cell_indices[index, :] = current_indices[cell_vertex_indices]
 
     return weights, cell_indices
+
+
+def determine_stack_dims(grid, variables):
+    all_dims = (tuple(grid[var].dims) for var in variables)
+
+    return tuple(dict.fromkeys(itertools.chain.from_iterable(all_dims)))
+
+
+def prepare_coords(grid, coords, stacked_dim, stacked_dims):
+    stacked = grid[coords].stack({stacked_dim: stacked_dims})
+
+    return np.stack([stacked[coord].data for coord in coords], axis=-1)
+
+
+def bilinear_interpolation_weights(
+    source_grid,
+    target_grid,
+    *,
+    n_neighbors=6,
+    metric="euclidean",
+    coords=["longitude", "latitude"],
+):
+    """xarray-aware bilinear interpolation weights computation"""
+    # TODO: how do we detect the variables and dims to stack?
+    # For now, just use the coords directly
+    source_stacked_dim = "_source_cells"
+    target_stacked_dim = "_target_cells"
+
+    # prepare the grids
+    source_stack_dims = determine_stack_dims(source_grid, coords)
+    source_coords = prepare_coords(
+        source_grid, coords, source_stacked_dim, source_stack_dims
+    )
+
+    target_stack_dims = determine_stack_dims(target_grid, coords)
+    target_coords = prepare_coords(
+        target_grid, coords, target_stacked_dim, target_stack_dims
+    )
+
+    common_dtype = np.common_type(source_coords, target_coords)
+
+    # use a tree index to find the n closest neighbors
+    tree = BallTree(source_coords, metric=metric)
+    _, neighbor_indices = tree.query(target_coords, k=n_neighbors)
+
+    # compute the weights
+    raw_weights, surrounding_vertex_indices = _compute_bilinear_interpolation_weights(
+        target_coords.astype(common_dtype),
+        source_coords.astype(common_dtype),
+        neighbor_indices,
+    )
+
+    # arrange as a sparse matrix
+    n_target = target_coords.shape[0]
+    n_source = source_coords.shape[0]
+
+    target_indices = np.broadcast_to(
+        np.arange(n_target)[:, None], surrounding_vertex_indices.shape
+    )
+    sparse_coords = np.stack([target_indices, surrounding_vertex_indices], axis=0)
+
+    source_shape = tuple([source_grid.sizes[dim] for dim in source_stack_dims])
+    target_shape = tuple([target_grid.sizes[dim] for dim in target_stack_dims])
+
+    raw_weights_matrix = sparse.COO(
+        np.reshape(sparse_coords, (2, -1)),
+        data=np.reshape(raw_weights, -1),
+        shape=(n_target, n_source),
+    )
+    weights_matrix = np.reshape(raw_weights_matrix, target_shape + source_shape)
+
+    # put into a DataArray
+    weights = xr.DataArray(
+        weights_matrix,
+        dims=target_stack_dims + source_stack_dims,
+        coords=target_grid.coords,
+        attrs={"sum_dims": source_stack_dims},
+    )
+
+    return weights
