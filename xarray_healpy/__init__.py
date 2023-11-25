b99da1682cb71f6c234dbbad2c76b0fb40639d9f
Justus Magin <keewis@posteo.de>
2023-11-25 18:55:13 +0100

    expose the main functionality in the top-level `__init__`

--- xarray_healpy/__init__.py
+++ xarray_healpy/__init__.py
@@ -1,5 +1,8 @@
 from importlib.metadata import version
 
+from xarray_healpy.grid import HealpyGridInfo  # noqa: F401
+from xarray_healpy.regridder import HealpyRegridder  # noqa: F401
+
 try:
     __version__ = version("xarray_healpy")
 except Exception:
